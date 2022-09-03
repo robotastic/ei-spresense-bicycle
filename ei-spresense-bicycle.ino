60b0478c8e5628d7d05db0e0310d1e0c68144093
Luke Berndt <lukekb@gmail.com>
2022-09-03 13:40:01 -0400

    Update ei-spresense-bicycle.ino

--- ei-spresense-bicycle.ino
+++ ei-spresense-bicycle.ino
@@ -29,13 +29,11 @@
 #include <SDHCI.h>
 #include <stdint.h>
 #include <stdlib.h>
-
+#include "edge-impulse-sdk/dsp/image/image.hpp"
 #define DEBUG true
 #define TAKE_PHOTOS true
 
 static SDClass theSD;
-#define DWORD_ALIGN_PTR(a)                                                     \
-  ((a & 0x3) ? (((uintptr_t)a + 0x4) & ~(uintptr_t)0x3) : a)
 
 /* Defines to center crop and resize a image to the Impulse image size the
    speresense hardware accelerator
@@ -61,10 +59,18 @@ static SDClass theSD;
 
 //#define RAW_WIDTH EI_CLASSIFIER_INPUT_WIDTH   // CAM_IMGSIZE_QVGA_H
 //#define RAW_HEIGHT EI_CLASSIFIER_INPUT_HEIGHT // CAM_IMGSIZE_QVGA_V
-
+/* Constant defines -------------------------------------------------------- */
 #define SCALE_FACTOR 1
 #define RAW_WIDTH CAM_IMGSIZE_QVGA_H
 #define RAW_HEIGHT CAM_IMGSIZE_QVGA_V
+#define EI_CAMERA_RAW_FRAME_BUFFER_COLS           CAM_IMGSIZE_QVGA_H
+#define EI_CAMERA_RAW_FRAME_BUFFER_ROWS           CAM_IMGSIZE_QVGA_V
+#define EI_CAMERA_RAW_FRAME_BYTE_SIZE             2
+
+
+
+
+
 #define CLIP_WIDTH (EI_CLASSIFIER_INPUT_WIDTH * SCALE_FACTOR)
 #define CLIP_HEIGHT (EI_CLASSIFIER_INPUT_HEIGHT * SCALE_FACTOR)
 #define OFFSET_X  ((RAW_WIDTH - CLIP_WIDTH) / 2)
@@ -83,6 +89,13 @@ static SDClass theSD;
 
 /* static variables */
 static CamImage sized_img;
+
+/*
+** @brief points to the output of the capture
+*/
+static uint8_t *ei_camera_capture_out = NULL;
+
+
 //float features[FEATURE_SIZE];
 
 static ei_impulse_result_t ei_result = {0};
@@ -94,69 +107,111 @@ void printError(enum CamErr err);
 void CamCB(CamImage img);
 
 
-/**
-   @brief      Convert monochrome data to rgb values
 
-   @param[in]  mono_data  The mono data
-   @param      r          red pixel value
-   @param      g          green pixel value
-   @param      b          blue pixel value
-*/
-static inline void mono_to_rgb(uint8_t mono_data, uint8_t *r, uint8_t *g,
-                               uint8_t *b) {
-  uint8_t v = mono_data;
-  *r = *g = *b = v;
-}
-/*
-int raw_feature_get_data(size_t offset, size_t length, float *out_ptr) {
-  memcpy(out_ptr, features + offset, length * sizeof(float));
-  return 0;
-}*/
+#define ALIGN_PTR(p,a)   ((p & (a-1)) ?(((uintptr_t)p + a) & ~(uintptr_t)(a-1)) : p)
 
-void r565_to_rgb(uint16_t color, uint8_t *r, uint8_t *g, uint8_t *b) {
-  *r = (color & 0xF800) >> 8;
-  *g = (color & 0x07E0) >> 3;
-  *b = (color & 0x1F) << 3;
-}
+/* Edge Impulse ------------------------------------------------------------- */
+
+typedef struct {
+    size_t width;
+    size_t height;
+} ei_device_resize_resolutions_t;
+
+
+
 
 
 /**
- * @brief      Convert RGB565 raw camera buffer to RGB888
+ * @brief      Convert rgb565 data to rgb888
  *
- * @param[in]   offset       pixel offset of raw buffer
- * @param[in]   length       number of pixels to convert
- * @param[out]  out_buf      pointer to store output image
+ * @param[in]  src_buf  The rgb565 data
+ * @param      dst_buf  The rgb888 data
+ * @param      src_len  length of rgb565 data
  */
 
-// this is from the Nano BLE example
-int ei_camera_cutout_get_data(size_t offset, size_t length, float *out_ptr) {
-    size_t pixel_ix = offset * 2; 
-    size_t bytes_left = length;
+bool RBG565ToRGB888(uint8_t *src_buf, uint8_t *dst_buf, uint32_t src_len)
+{
+    uint8_t hb, lb;
+    uint32_t pix_count = src_len / 2;
+    uint32_t count = 0;
+
+      ei_printf("\timg size: %u\n", src_len);
+
+    
+    for(uint32_t i = 0; i < pix_count; i ++) {
+        hb = *src_buf++;
+        lb = *src_buf++;
+
+        *dst_buf++ = hb & 0xF8;
+        *dst_buf++ = (hb & 0x07) << 5 | (lb & 0xE0) >> 3;
+        *dst_buf++ = (lb & 0x1F) << 3;
+        count++;
+        ei_printf("Src: %u\n",count); 
+    }
+
+    return true;
+}
+
+static int ei_camera_get_data(size_t offset, size_t length, float *out_ptr)
+{
+    // we already have a RGB888 buffer, so recalculate offset into pixel index
+    size_t pixel_ix = offset * 3;
+    size_t pixels_left = length;
     size_t out_ptr_ix = 0;
-     // grab the value and convert to r/g/b
-    uint8_t *buffer = sized_img.getImgBuff();
-    // read byte for byte
-    while (bytes_left != 0) {
 
-        uint16_t pixel = (buffer[pixel_ix] << 8) | buffer[pixel_ix+1];
-        uint8_t r, g, b;
-        r = ((pixel >> 11) & 0x1f) << 3;
-        g = ((pixel >> 5) & 0x3f) << 2;
-        b = (pixel & 0x1f) << 3;
+    while (pixels_left != 0) {
+        out_ptr[out_ptr_ix] = (ei_camera_capture_out[pixel_ix] << 16) + (ei_camera_capture_out[pixel_ix + 1] << 8) + ei_camera_capture_out[pixel_ix + 2];
 
-        // then convert to out_ptr format
-        float pixel_f = (r << 16) + (g << 8) + b;
-        out_ptr[out_ptr_ix] = pixel_f;
-
-        // and go to the next pixel
+        // go to the next pixel
         out_ptr_ix++;
-        pixel_ix+=2;
-        bytes_left--;
+        pixel_ix+=3;
+        pixels_left--;
     }
 
     // and done!
     return 0;
 }
+
+/**
+ * @brief      Determine whether to resize and to which dimension
+ *
+ * @param[in]  out_width     width of output image
+ * @param[in]  out_height    height of output image
+ * @param[out] resize_col_sz       pointer to frame buffer's column/width value
+ * @param[out] resize_row_sz       pointer to frame buffer's rows/height value
+ * @param[out] do_resize     returns whether to resize (or not)
+ *
+ */
+int calculate_resize_dimensions(uint32_t out_width, uint32_t out_height, uint32_t *resize_col_sz, uint32_t *resize_row_sz, bool *do_resize)
+{
+    size_t list_size = 6;
+    const ei_device_resize_resolutions_t list[list_size] = {
+        {64, 64},
+        {96, 96},
+        {160, 120},
+        {160, 160},
+        {320, 240},
+    };
+
+    // (default) conditions
+    *resize_col_sz = EI_CAMERA_RAW_FRAME_BUFFER_COLS;
+    *resize_row_sz = EI_CAMERA_RAW_FRAME_BUFFER_ROWS;
+    *do_resize = false;
+
+    for (size_t ix = 0; ix < list_size; ix++) {
+        if ((out_width <= list[ix].width) && (out_height <= list[ix].height)) {
+            *resize_col_sz = list[ix].width;
+            *resize_row_sz = list[ix].height;
+            *do_resize = true;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+
+
 
 /**
    Print error message
@@ -199,83 +254,6 @@ void printError(enum CamErr err) {
   }
 }
 
-/**
-   @brief run inference on the static sized_image buffer using the provided
-   impulse
-*/
-static void ei_wildlife_camera_classify(bool debug) {
-  ei::signal_t signal;
-  spotted_object = false;
-  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
-  signal.get_data = &ei_camera_cutout_get_data; //&raw_feature_get_data; //&ei_camera_cutout_get_data;
-
-  score = 0;
-  EI_IMPULSE_ERROR err = run_classifier(&signal, &ei_result, DEBUG_NN);
-  if (err != EI_IMPULSE_OK) {
-    ei_printf("ERROR: Failed to run classifier (%d)\n", err);
-    return;
-  }
-  // print the predictions
-  if (debug) {
-    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d "
-              "ms.): \n",
-              ei_result.timing.dsp, ei_result.timing.classification,
-              ei_result.timing.anomaly);
-#if EI_CLASSIFIER_OBJECT_DETECTION == 1
-    bool bb_found = ei_result.bounding_boxes[0].value > 0;
-
-    LED1_OFF
-    LED2_OFF
-    LED3_OFF
-
-    spotted_object = true;
-    
-    for (size_t ix = 0; ix < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; ix++) {
-      auto bb = ei_result.bounding_boxes[ix];
-      if (bb.value > score) {
-        score = bb.value;
-      }
-      
-      if (bb.value == 0) {
-        continue;
-      }
-      if (bb.value > 0.2) {
-        spotted_object = true;
-      }
-      if (bb.value > 0.7) {
-        LED1_ON
-      }
-      if (bb.value > 0.8) {
-        LED2_ON
-      }
-      if (bb.value > 0.9) {
-        LED3_ON
-      }
-      ei_printf("    %s (", bb.label);
-      ei_printf_float(bb.value);
-      ei_printf(") [ x: %u, y: %u, width: %u, height: %u ]\n", bb.x, bb.y,
-                bb.width, bb.height);
-    }
-
-    if (!bb_found) {
-      ei_printf("    No objects found\n");
-    }
-#else
-    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
-      ei_printf("    %s: ", ei_result.classification[ix].label);
-      ei_printf_float(ei_result.classification[ix].value);
-      ei_printf("\n");
-    }
-#if EI_CLASSIFIER_HAS_ANOMALY == 1
-    ei_printf("    anomaly score: ");
-    ei_printf_float(ei_result.anomaly);
-    ei_printf("\n");
-#endif
-#endif
-  }
-
-  return;
-}
 
 /**
  * @brief callback that checks for the presence of an animal in the camera
@@ -312,7 +290,7 @@ void ei_wildlife_camera_start_continuous(bool debug) {
   // still image format must be jpeg to allow for compressed storage/transmit
 
   err = theCamera.setStillPictureImageFormat(RAW_WIDTH, RAW_HEIGHT,
-                                             CAM_IMAGE_PIX_FMT_YUV422); //CAM_IMAGE_PIX_FMT_RGB565);
+                                             CAM_IMAGE_PIX_FMT_RGB565); //CAM_IMAGE_PIX_FMT_YUV422); //CAM_IMAGE_PIX_FMT_RGB565);
   // CAM_IMAGE_PIX_FMT_YUV422);
   // CAM_IMAGE_PIX_FMT_RGB565);
   // CAM_IMAGE_PIX_FMT_GRAY);
@@ -323,65 +301,85 @@ void ei_wildlife_camera_start_continuous(bool debug) {
   Serial.println("INFO: started wildlife camera recording");
 }
 
-void ei_capture_classify() {
+/**
+   @brief run inference on the static sized_image buffer using the provided
+   impulse
+*/
+static void ei_wildlife_camera_classify(bool debug) {
+
+}
+
+
+
+
+bool ei_camera_capture(uint32_t img_width, uint32_t img_height) {
   CamErr err;
-  char filename[400];
+
   bool debug = true;
+      bool do_resize = false;
+    bool do_crop = false;
+
+ei_printf("Start Capture\n");
+
+    
+    //ei_camera_capture_out = (uint8_t *)ALIGN_PTR((uintptr_t)ei_camera_capture_out, 32);
+    
   // snapshot and save a jpeg
+    unsigned long StartTime = millis();
   CamImage img = theCamera.takePicture();
 
   if (!img.isAvailable())
     return; // fast path if image is no longer ready
 
-  ei_printf("resize:\n");
-  err = img.clipAndResizeImageByHW(sized_img
-                                   , OFFSET_X, OFFSET_Y
-                                   , OFFSET_X + CLIP_WIDTH - 1
-                                   , OFFSET_Y + CLIP_HEIGHT - 1
-                                   , EI_CLASSIFIER_INPUT_WIDTH, EI_CLASSIFIER_INPUT_HEIGHT);
-  if (err) printError(err);
-  err = sized_img.convertPixFormat(CAM_IMAGE_PIX_FMT_RGB565);
-  if (err) printError(err);
-  /*memcpy(features, img.getImgBuff(), img.getImgSize());
+    bool converted = RBG565ToRGB888(img.getImgBuff(), ei_camera_capture_out, img.getImgSize());
+    
+    if(!converted){
+        ei_printf("ERR: Conversion failed\n");
+        return false;
+    } else {
+      ei_printf("ESuccessfully converted to RGB888\n");
+    }
+        
+    uint32_t resize_col_sz;
+    uint32_t resize_row_sz;
+    // choose resize dimensions
+    /*int res = calculate_resize_dimensions(img_width, img_height, &resize_col_sz, &resize_row_sz, &do_resize);
+    ei_printf("Resize results: Raw Image Width: %d Height: %d Resize Col: %d, Resize Row: %d, Do Resize: %d\n", img_width, img_height, resize_col_sz, resize_row_sz, do_resize);
+    if (res) {
+        ei_printf("ERR: Failed to calculate resize dimensions (%d)\r\n", res);
+        return false;
+    }
 
-  uint8_t r, g, b;
-  for (int i = 0; i < FEATURE_SIZE; i++) {
-    r565_to_rgb(features[i], &r, &g, &b);
-    // then convert to out_ptr format
-    float pixel_f = (r << 16) + (g << 8) + b;
-    features[i] = pixel_f;
-  }*/
+    if ((img_width != resize_col_sz)
+        || (img_height != resize_row_sz)) {
+        do_crop = true;
+    }
+*/
 
-ei_wildlife_camera_classify(true);
+    //if (do_resize) {
+/*
+          ei::image::processing::crop_and_interpolate_rgb888(
+          ei_camera_capture_out,
+          EI_CAMERA_RAW_FRAME_BUFFER_COLS,
+          EI_CAMERA_RAW_FRAME_BUFFER_ROWS,
+          ei_camera_capture_out,
+          img_width,
+          img_height);*/
+          //resize_col_sz,
+          //resize_row_sz);
+    //}
 
-  LED0_ON
 
-  if (spotted_object && theSD.begin() && sized_img.isAvailable()) {
-    int short_score = floor(score * 100);
-    /*sprintf(filename, "%d-%d.rgb", take_picture_count, short_score);
-    if (debug)
-      ei_printf("INFO: saving %s to SD card... size: %d", filename, sizeof(features));
-    theSD.remove(filename);
-    File myFile = theSD.open(filename, FILE_WRITE);
-    myFile.write((const char*) features, sizeof(features));
-    myFile.close();*/
 
-    sprintf(filename, "%d-%d.565", take_picture_count, short_score);
-    if (debug)
-      ei_printf("INFO: saving %s to SD card... size: %d\n", filename, sized_img.getImgSize());
-    theSD.remove(filename);
-    File myFile = theSD.open(filename, FILE_WRITE);
-    myFile.write(sized_img.getImgBuff() ,sized_img.getImgSize());
-    myFile.close();
-  } else if (debug) {
-    Serial.println("failed to compress and save image, check that camera and "
-                   "SD card are connected properly");
-  }
-  LED0_OFF
+  //ei_wildlife_camera_classify(true);
+  unsigned long CurrentTime = millis();
+  unsigned long ElapsedTime = CurrentTime - StartTime;
+  ei_printf("Capture and Convert Time: %u ms \n", ElapsedTime);
+
 
   
-
-
+  //ei_free(ei_camera_capture_out);
+ei_printf("End Capture\n");
   take_picture_count++;
 }
 
@@ -435,6 +433,9 @@ void setup() {
   ei_printf("Enabled EI_CLASSIFIER_TFLITE_ENABLE_CMSIS_NN : %d\n",
             EI_CLASSIFIER_TFLITE_ENABLE_CMSIS_NN);
 #endif
+ei_camera_capture_out = (uint8_t*)ei_malloc(EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * 3 + 32);
+
+ei_printf("Capture Buffer Size: %d \n", EI_CAMERA_RAW_FRAME_BUFFER_COLS * EI_CAMERA_RAW_FRAME_BUFFER_ROWS * 3 + 32);
   /*LowPower.begin();
  // Set the highest clock mode
  printClockMode();
@@ -452,7 +453,107 @@ printClockMode();*/
 }
 
 void loop() {
-  // ei_capture_classify();
-  ei_capture_classify();
-  // sleep(500);
+  ei_printf("Start Loop\n");
+
+  ei::signal_t signal;
+  spotted_object = false;
+  char filename[400];
+  
+ ei_printf("Start Classify\n");
+  signal.total_length = EI_CLASSIFIER_INPUT_WIDTH * EI_CLASSIFIER_INPUT_HEIGHT;
+  signal.get_data = &ei_camera_get_data; 
+  score = 0;
+
+    if (ei_camera_capture((size_t)EI_CLASSIFIER_INPUT_WIDTH, (size_t)EI_CLASSIFIER_INPUT_HEIGHT) == false) {
+        ei_printf("Failed to capture image\r\n");
+        return;
+    }
+
+  /*
+  EI_IMPULSE_ERROR err = run_classifier(&signal, &ei_result, DEBUG_NN);
+  if (err != EI_IMPULSE_OK) {
+    ei_printf("ERROR: Failed to run classifier (%d)\n", err);
+    return;
+  }
+  // print the predictions
+
+    ei_printf("Predictions (DSP: %d ms., Classification: %d ms., Anomaly: %d "
+              "ms.): \n",
+              ei_result.timing.dsp, ei_result.timing.classification,
+              ei_result.timing.anomaly);
+
+              
+#if EI_CLASSIFIER_OBJECT_DETECTION == 1
+    bool bb_found = ei_result.bounding_boxes[0].value > 0;
+
+    LED1_OFF
+    LED2_OFF
+    LED3_OFF
+
+    spotted_object = true;
+    
+    for (size_t ix = 0; ix < EI_CLASSIFIER_OBJECT_DETECTION_COUNT; ix++) {
+      auto bb = ei_result.bounding_boxes[ix];
+      if (bb.value > score) {
+        score = bb.value;
+      }
+      
+      if (bb.value == 0) {
+        continue;
+      }
+      if (bb.value > 0.2) {
+        spotted_object = true;
+      }
+      if (bb.value > 0.7) {
+        LED1_ON
+      }
+      if (bb.value > 0.8) {
+        LED2_ON
+      }
+      if (bb.value > 0.9) {
+        LED3_ON
+      }
+      ei_printf("    %s (", bb.label);
+      ei_printf_float(bb.value);
+      ei_printf(") [ x: %u, y: %u, width: %u, height: %u ]\n", bb.x, bb.y,
+                bb.width, bb.height);
+    }
+
+    if (!bb_found) {
+      ei_printf("    No objects found\n");
+    }
+#else
+    for (size_t ix = 0; ix < EI_CLASSIFIER_LABEL_COUNT; ix++) {
+      ei_printf("    %s: ", ei_result.classification[ix].label);
+      ei_printf_float(ei_result.classification[ix].value);
+      ei_printf("\n");
+    }
+#if EI_CLASSIFIER_HAS_ANOMALY == 1
+    ei_printf("    anomaly score: ");
+    ei_printf_float(ei_result.anomaly);
+    ei_printf("\n");
+#endif
+#endif
+*/
+  ei_printf("End Classify\n");
+  LED0_ON
+/*
+  if (spotted_object && theSD.begin() && sized_img.isAvailable()) {
+    int short_score = floor(score * 100);
+
+    sprintf(filename, "%d-%d.565", take_picture_count, short_score);
+    if (debug)
+      ei_printf("INFO: saving %s to SD card... size: %d\n", filename, sized_img.getImgSize());
+    theSD.remove(filename);
+    File myFile = theSD.open(filename, FILE_WRITE);
+    myFile.write(sized_img.getImgBuff() ,sized_img.getImgSize());
+    myFile.close();
+  } else if (debug) {
+    Serial.println("failed to compress and save image, check that camera and "
+                   "SD card are connected properly");
+  }*/
+  LED0_OFF
+
+
+ei_printf("End Loop\n");
 }
